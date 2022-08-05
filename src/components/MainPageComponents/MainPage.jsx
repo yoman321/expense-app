ff14ee97713e9f94dce07a83c3a0dc489314a725
yoman3215 <luophilip@outlook.com>
2022-08-05 14:35:06 -0400

    added about this app section in MainPage

--- src/components/MainPageComponents/MainPage.jsx
+++ src/components/MainPageComponents/MainPage.jsx
@@ -1,29 +1,46 @@
 import { Link } from "react-router-dom";
+import AppPic from "../../images/some-profile-pic.jpg";
 import styles from "./MainPage.module.css";
 
 const MainPage = () => {
   return (
     <div className={styles.main}>
-      <h1>Welcome to Expense Report</h1>
-      <div className={styles.actions}>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <button>
-                  <Link to="/graph">To Graph</Link>
-                </button>
-              </td>
-              <td>
-                <button>
-                  <Link to="/expense-list">To Expense List</Link>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className={styles.section}>
+        <h1>Welcome to Expense Report</h1>
+        <div className={styles.actions}>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <button>
+                    <Link to="/graph">To Graph</Link>
+                  </button>
+                </td>
+                <td>
+                  <button>
+                    <Link to="/expense-list">To Expense List</Link>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className={styles.section}></div>
+      <div className={styles.section}>
+        <h2>About this App</h2>
+        <div className={styles.content}>
+          <div className={styles.image}>
+            <img src={AppPic} alt="App Pic"></img>
+          </div>
+          <section className={styles.text}>
+            <p>
+              This app has been created by students at McGill University in
+              order to help students such as themselves keep track of their
+              monthly expenses and help with their monthly budget.
+            </p>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
